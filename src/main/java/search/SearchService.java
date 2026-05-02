f51de09602236a0055caa399b652feded75b3f27
ericklucioh <erickrealoficial@gmail.com>
2026-05-01 22:20:59 -0300

    [main] remove old cli

--- src/main/java/search/SearchService.java
+++
@@ -1,8 +0,0 @@
-package thread.search;
-
-import java.nio.file.Path;
-import java.util.List;
-
-public interface SearchService {
-    List<SearchResult> search(List<Path> datasetDirectories, String target);
-}
